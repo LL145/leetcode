ca26c1419080ff7a26829b09e6d6c557be86a25a
LL145 <44174238+LL145@users.noreply.github.com>
2021-12-11 01:34:31 -0500

    Update convert.py

--- 6. Zigzag Conversion/convert.py
+++ 6. Zigzag Conversion/convert.py
@@ -1,14 +1,10 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows >= len(s):
-            return s
-
-        rows= [''] * numRows
-        r = 0
-        d = -1
+        res = ['' for _ in range(numRows)]
+        curr_row = 0
         for c in s:
-            rows[r] += c
-            if r ==0 or r == numRows-1:
-                d = -d
-            r += d
-        return ''.join(rows)
+            res[curr_row] += c
+            if curr_row == 0: direction = 1
+            elif curr_row == numRows - 1: direction = -1
+            curr_row = (direction + curr_row) % numRows
+        return ''.join(res)
