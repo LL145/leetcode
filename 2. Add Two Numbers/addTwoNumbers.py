d85f2aea51a2279b79d6333533a33b901152ac56
LL145 <44174238+LL145@users.noreply.github.com>
2021-12-13 14:33:51 -0500

    Update addTwoNumbers.py

--- 2. Add Two Numbers/addTwoNumbers.py
+++ 2. Add Two Numbers/addTwoNumbers.py
@@ -1,30 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        #result = ListNode()
-        #result_tail = result
-        #carry = 0
-        
-        if l1 or l2:
-            val1  = (l1.val if l1 else 0)
-            val2  = (l2.val if l2 else 0)
-            carry, out = divmod(val1+val2, 10)
-            result = ListNode(out)
-            result_tail = result
-            l1 = (l1.next if l1 else None)
-            l2 = (l2.next if l2 else None)
-        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = ListNode()
+        curr = root
+        carry = 0
         while l1 or l2 or carry:
-            val1  = (l1.val if l1 else 0)
-            val2  = (l2.val if l2 else 0)
-            carry, out = divmod(val1+val2 + carry, 10)
-            result_tail.next = ListNode(out)
-            result_tail = result_tail.next
-            
-            l1 = (l1.next if l1 else None)
-            l2 = (l2.next if l2 else None)
-        return result
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            carry, out = divmod(v1 + v2 + carry, 10)
+            curr.next = ListNode(val = out)
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return root.next
